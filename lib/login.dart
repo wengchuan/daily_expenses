67cdcbbbe7b22aae1e25957fe9ac38d4989b4feb
wengchuan <wengchuankot@gmail.com>
2023-12-12 10:38:41 +0800

    Added image into login

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:daily_expenses/enter_ip.dart';
 import 'package:flutter/material.dart';
 import 'dailyexpenses.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -33,58 +34,69 @@ class _LoginScreenState extends State<LoginScreen> {
         title: const Text("Login Screen"),
       ),
       body:
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: usernameController,
-                decoration: const InputDecoration(
-                  labelText: "Username",
+      SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                    image: "https://w7.pngwing.com/pngs/978/821/"
+                        "png-transparent-money-finance-wallet-payment-daily"
+                        "-expenses-saving-service-personal-finance.png"),
+
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    labelText: "Username",
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: "Password",
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                  ),
                 ),
               ),
-            ),
-            ElevatedButton(onPressed: (){
-                String username = usernameController.text;
-                String password = passwordController.text;
-                if(username == "test" && password=="123456789"){
-                  Navigator.push(context, MaterialPageRoute(builder:
-                  (context)=>DailyExpenseApp(username: username,),
-                  ));
-
-                }
-                else{
-                  showDialog(context: context, builder: (context){
-                    return AlertDialog(
-                      title: const Text("Login Failed"),
-                      content: const Text("Invalid username or password"),
-                      actions: [
-                        TextButton(onPressed: (){
-                          Navigator.pop(context);
-                        }, child: const Text("OK"))
-                      ],
-                    );
-                  });
-
+              ElevatedButton(onPressed: (){
+                  String username = usernameController.text;
+                  String password = passwordController.text;
+                  if(username == "test" && password=="123456789"){
+                    Navigator.push(context, MaterialPageRoute(builder:
+                    (context)=>DailyExpenseApp(username: username,),
+                    ));
 
                   }
-            },
-                child: const Text("Login")),
+                  else{
+                    showDialog(context: context, builder: (context){
+                      return AlertDialog(
+                        title: const Text("Login Failed"),
+                        content: const Text("Invalid username or password"),
+                        actions: [
+                          TextButton(onPressed: (){
+                            Navigator.pop(context);
+                          }, child: const Text("OK"))
+                        ],
+                      );
+                    });
 
-          ],
+
+                    }
+              },
+                  child: const Text("Login")),
+
+            ],
+          ),
+
         ),
-
       ),
 
     );
